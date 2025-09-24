186dc70e8514f6948e97399cb9ced30aba78f296
Gamonymous <33094562+Rchardon@users.noreply.github.com>
2025-09-24 16:08:40 +0200

    Admin redirection

--- src/Controller/ChatController.php
+++ src/Controller/ChatController.php
@@ -5,12 +5,18 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\Security;
 
 final class ChatController extends AbstractController
 {
     #[Route('/chat', name: 'app_chat')]
     public function index(): Response
     {
+        $user = $this->getUser();
+        if(in_array('ROLE_ADMIN', $user->getRoles())){
+            return $this->redirectToRoute('admin');
+        }
+
         return $this->render('chat-unified.html.twig', [
             'controller_name' => 'ChatController',
         ]);
